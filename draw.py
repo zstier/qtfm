bb217138aff4af278fb3a52e2ce2a0b62b6d67d8
Carlos Esparza <ca2010es@gmail.com>
2022-09-01 21:59:28 -0700

    'cleanup;

--- draw.py
+++ draw.py
@@ -1,8 +1,6 @@
 #!/bin/env python3
 import numpy as np
 
-# import random
-
 np.random.seed(835845000375724 % (2**32))
 
 with open("speakers.txt") as file:
@@ -18,26 +16,3 @@ weights /= sum(weights)
 
 # print(random.choices(attendees, weights=weights, k=3))
 print(np.random.choice(attendees, size=3, replace=False, p=weights))
-
-"""
-import numpy as np
-vec=[1,2,3]
-P=[0.5,0.2,0.3]
-np.random.choice(vec,size=2,replace=False, p=P)
-"""
-
-"""
-tot = 0
-for s in attendees.keys():
-	tot += attendees[s]
-for s in attendees.keys():
-	attendees[s] /= tot
-
-val = random()
-tally = 0
-for s in attendees.keys():
-	tally += attendees[s]
-	if tally > val:
-		print("speaker is", s)
-		break
-"""
