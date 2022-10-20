acd5e01a7d7e16afb180786bd5926dd1ea4686b8
Carlos Esparza <ca2010es@gmail.com>
2022-10-19 22:41:46 -0700

    added functionality for missing while boosted (also a bunck of black formatting changes)

--- draw.py
+++ draw.py
@@ -3,6 +3,8 @@ import sys, io
 import numpy as np
 import re
 from datetime import datetime as dt
+from functools import reduce
+from operator import or_
 
 BETA = np.log(2)
 EMAIL = "quantumcroupier@gmail.com"
@@ -14,7 +16,7 @@ RNGBOT_URL = "https://twitter.com/rndgenbot"
 terminal = print
 _print = print
 
-if sys.argv[1:] and sys.argv[1] == '--dry':
+if sys.argv[1:] and sys.argv[1] == "--dry":
     DRY = True
     sys.argv.pop(1)
 else:
@@ -27,7 +29,7 @@ if sys.argv[2:]:
 
     def print(*args, **kwargs):
         terminal(*args, **kwargs)
-        _print( *args, **kwargs, file=mail)
+        _print(*args, **kwargs, file=mail)
 
     croupier = lambda *args, **kwargs: terminal(*args, **kwargs, file=mail)
 else:
@@ -50,38 +52,40 @@ print("=" * 10 + f" Week {week} " + "=" * 10)
 print(f"It is {dt.now().strftime('%Y-%m-%d %H:%M:%S')}.")
 
 with open("miss.txt") as f:
-    miss = [[x for x in l.strip().split(",") if x] for l in f.readlines() if not l.startswith("#")]
-    # Now let's do a sanity check!
-    assert (
-        len(miss) >= week
-    ), "miss.txt needs to have at least (# past talks) + 1 lines"
-    miss = miss[week-1]
+    misses = [{x for x in l.strip().split(",") if x} for l in f.readlines()]
+    # sanity check
+    assert len(misses) >= week, "miss.txt missing line for this week"
+    misses = misses[:week]
+    missing = misses[week - 1]
 
 with open("zeros.txt") as f:
-    zeros = [[x for x in l.strip().split(",") if x] for l in f.readlines() if not l.startswith("#")]
-    # Now let's do a bunch of sanity checks!
-    assert (
-        len(zeros) >= week
-    ), "zeros.txt needs to have at least (# past talks) + 1 lines"
+    zeros = [{x for x in l.strip().split(",") if x} for l in f.readlines()]
+    # sanity check
+    assert len(zeros) >= week, "zeros.txt missing line for this week"
     assert not any(zeros[week:]), "people have zeroed out for future weeks??"
     zeros = zeros[:week]
 
 for pers in set(speakers) - set(attendees):
     print(f"WARN: speaker {pers} is not attendee")
-for pers in {p for wk in zeros for p in wk} - set(attendees):
+for pers in reduce(or_, zeros, set()) - set(attendees):
     print(f'WARN: zeroed-out person "{pers}" is not attendee')
-for pers in set(miss) - set(attendees):
+for pers in reduce(or_, misses, set()) - set(attendees):
     print(f'WARN: missing person "{pers}" is not attendee')
 
-llz, lz, z = [[], [], *zeros][-3:]
-if llz: print("weight doubled for zeroing 2 weeks ago: " + ", ".join(llz))
-if lz:  print("weight doubled for zeroing last week:   " + ", ".join(lz))
-if z:   print("zeroed this week: " + ", ".join(z))
-if miss: print("missing this week: " + ", ".join(miss))
-assert len(set(z + lz + llz)) == len(z + lz + llz), "someone zeroed out too often!!"
+z = zeros[-1]
+llz, lz = set(), set()
+for zj, mj in zip(zeros[:-1], misses):
+    assert not zj & mj, "People missing don't need to zero out: " + ", ".join(zj & mj)
+    assert not (intr := zj & (lz | llz)), "zeroing while boosted:" + ", ".join(intr)
+
+    llz = (llz & mj) | (lz - mj)
+    lz &= (lz & mj) | zj # lz for next week!
+
+if lz: print("weight doubled for zeroing (first time): " + ", ".join(lz - missing))
+if llz: print("weight doubled for zeroing (second time): " + ", ".join(llz - missing))
+if z: print("zeroed this week: " + ", ".join(z))
+if missing: print("missing this week: " + ", ".join(missing))
 print()
-assert not set(z) & set(miss), "People missing don't need to zero out"
-assert not (set(lz) | set(llz)) & set(miss), "It looks like someone might be trying to escape their increased weight"
 
 if sys.argv[1:]:
     import tweepy
@@ -94,7 +98,7 @@ if sys.argv[1:]:
     seed_base = ""
     for tw in tweets:
         (a, b) = map(int, *re.findall(r"numbers are (\d{,2}) and (\d{,3})!", tw.text))
-        seed_base += f'{a:02}{b:03}'
+        seed_base += f"{a:02}{b:03}"
 
     print()
     print(f"base seed is b={seed_base}")
@@ -118,16 +122,16 @@ count = np.fromiter(map(speakers.count, attendees), dtype=int)
 
 weights = (
     np.exp(-BETA * count)
-    * (attendees != speakers[-1])  # speaker gets weight 0
-    * 2 ** np.isin(attendees, lz)  # x2 weight for ppl who zeroed in the past 2 weeks
-    * 2 ** np.isin(attendees, llz)
-    * (1 - np.isin(attendees, z))  # zero out
-    * (1 - np.isin(attendees, miss))  # missing
+    * (attendees != speakers[-1])  # last speaker gets weight 0
+    * 2 ** np.isin(attendees, list(lz)) # boost (1st time)
+    * 2 ** np.isin(attendees, list(llz)) # boost (2nd time)
+    * (1 - np.isin(attendees, list(z)))  # zero out
+    * (1 - np.isin(attendees, list(missing)))  # missing
 )
 weights /= sum(weights)
 
 pad = max(14, *(len(p) for p in attendees))
-croupier('<pre style="font-size: 13px">', end='')
+croupier('<pre style="font-size: 13px">', end="")
 print("🎲 Participant", " " * (pad - 11), "prob  🎲")
 print("-" * (5 + pad + 8))
 for i, (pers, prob) in enumerate(zip(attendees, weights)):
@@ -138,13 +142,13 @@ for i, (pers, prob) in enumerate(zip(attendees, weights)):
         f"{prob:.3f}",
         ["⬛", "🟥"][(i + 1) % 2],
     )
-croupier('</pre>', end='')
+croupier("</pre>", end="")
 
 winners = np.random.choice(attendees, size=3, replace=False, p=weights)
 print(f"\nAnd the winner is ... ", end="")
-croupier('<marquee><b>', end='')
-print(f"🎉 {winners[0]} 🎉", end='')
-croupier('</marquee></b>', end='')
+croupier("<marquee><b>", end="")
+print(f"🎉 {winners[0]} 🎉", end="")
+croupier("</marquee></b>", end="")
 print()
 croupier(
     "If you can't talk tomorrow for some reason please reply-all ASAP so that the backup speaker may prepare"
@@ -158,12 +162,16 @@ for n, pers in enumerate(winners[1:]):
 if twitter:
     print()
     for i, tw in enumerate(tweets):
-        terminal(f'{[i+1]} https://twitter.com/rndgenbot/status/{tw.id}')
+        terminal(f"{[i+1]} https://twitter.com/rndgenbot/status/{tw.id}")
 
-croupier('Alea iacta est,\nthe 🎲<a href="https://github.com/zstier/qtfm">Quantum Croupier</a>🎲')
+croupier(
+    'Alea iacta est,\nthe 🎲<a href="https://github.com/zstier/qtfm">Quantum Croupier</a>🎲'
+)
 
 # send email -------------------------------------------------------------------
-SUBJECT = "[DRY] " * DRY + "The 🎲Quantum Croupier🎲 is pleased to announce tomorrow's speaker."
+SUBJECT = (
+    "[DRY] " * DRY + "The 🎲Quantum Croupier🎲 is pleased to announce tomorrow's speaker."
+)
 
 if not email:
     exit()
@@ -180,13 +188,16 @@ with open("emails.txt") as f:
 text = (
     '<font style="font-size: 13px" face="DejaVu Sans Mono, Courier, Courier New, monospace">'
     + mail.getvalue()
-        .replace("\n", "<br>")
-        .replace("@rndgenbot", f'<a href="{RNGBOT_URL}">@rndgenbot</a>')
+    .replace("\n", "<br>")
+    .replace("@rndgenbot", f'<a href="{RNGBOT_URL}">@rndgenbot</a>')
     + "</font>"
 )
 # text = re.sub(r"(🎉.*🎉)", "<marquee><b>\\1</b></marquee>", text)
 for i, tw in enumerate(tweets):
-    text = text.replace(f'[{i+1}]', f'<a href="https://twitter.com/rndgenbot/status/{tw.id}">[{i+1}]</a>')
+    text = text.replace(
+        f"[{i+1}]",
+        f'<a href="https://twitter.com/rndgenbot/status/{tw.id}">[{i+1}]</a>',
+    )
 
 message = MIMEMultipart()
 message["From"] = "quantumcroupier@math.berkeley.edu"
